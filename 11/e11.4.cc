7c4c22f5089adae3fdf25968ba33797448ec599f
pendulm <lonependulm@gmail.com>
2013-05-01 23:36:43 +0800

    exercise 11.4

---
+++ 11/e11.4.cc
@@ -0,0 +1,49 @@
+#include <map>
+using std::map;
+#include <string>
+using std::string;
+#include <set>
+using std::set;
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <cctype>
+using std::tolower;
+
+void lower_and_strip(string& s) {
+  if (s.empty())
+    return;
+
+  auto l = s.begin(), r = s.end();
+  while (l < r && ispunct(*l))
+    ++l;
+  while (l < r && ispunct(*(r-1)))
+    --r;
+
+  // must remove tail first, otherwise the iterator will be invalid
+  s.erase(r, s.end());
+  s.erase(s.begin(), l);
+
+  for (auto& c : s) {
+    c = tolower(c);
+  }
+}
+
+int main() {
+  set<string> exclude = {"the", "but", "and", "or", "an", "a"};
+  map<string, size_t> word_count;
+  string s;
+  while (cin >> s) {
+    // s is the same object, so erase wouldn't change memory allocate
+    lower_and_strip(s);
+    if (exclude.find(s) == exclude.end() && !s.empty()) {
+      word_count[s] += 1;
+    }
+  }
+  for (const auto& pair : word_count) {
+    cout << pair.first << ":" << pair.second << endl;
+  }
+  return 0;
+}
+    
